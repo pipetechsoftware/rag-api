ae74b9c6778e5f8c0d2534b5694ff0c69928c6a4
williancae <williancaecam@gmail.com>
2025-02-20 12:19:12 -0300

    DONE: conclusão API de comunicação com Qdrant

--- interfaces/qdrant_interface.py
+++ interfaces/qdrant_interface.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 
 class MetadataInterface(BaseModel):
     index: int
-    agent_id: str
+    agent_id: int
 
 class DocumentInterface(BaseModel):
     content: str
@@ -11,6 +11,6 @@ class DocumentInterface(BaseModel):
 
 class ResponseInterface(BaseModel):
     id: str
-    agent_id: str
+    agent_id: int
     page_content: str
     similarity: float
--- jobs/upload_qdrant_job.py
+++ jobs/upload_qdrant_job.py
@@ -9,7 +9,7 @@ extract = ExtractService()
 qdrant_service = QdrantService()
 
 
-def upload_qdrant_job(job_id: str, agent_id: str, file: bytes) -> None:
+def upload_qdrant_job(job_id: str, agent_id: int, file: bytes) -> None:
     response: List[str] = extract.run(source=file)
     documents: List[DocumentInterface] = [ DocumentInterface(content=doc, metadata=MetadataInterface(index=i, agent_id=agent_id) ) for i, doc in enumerate(response) ]
     qdrant_service.insert_documents(
